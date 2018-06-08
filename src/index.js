8ac4dc49a86e1e0f7584621c505394dee7ea5829
carloscarvallo <carloscarvallo@protonmail.com>
2018-06-08 13:37:13 -0400

    search bar implemented

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
-const API_KEY = '';
+const API_KEY = 'asd';
 
 class App extends Component {
     constructor (props) {
@@ -15,8 +15,15 @@ class App extends Component {
             selectedVideo: null
         };
 
-        YTSearch({ key: API_KEY, term: 'ola englund'}, videos => {
-            this.setState({ 
+        this.videoSearch('ola englund');
+    }
+
+    videoSearch (term) {
+        YTSearch({
+            key: API_KEY,
+            term: term
+        }, videos => {
+            this.setState({
                 videos: videos,
                 selectedVideo: videos[0]
             })
@@ -26,7 +33,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <SearchBar />
+                <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
                 <VideoDetail video={this.state.selectedVideo} />
                 <VideoList 
                     onVideoSelect={selectedVideo => this.setState({selectedVideo})}
